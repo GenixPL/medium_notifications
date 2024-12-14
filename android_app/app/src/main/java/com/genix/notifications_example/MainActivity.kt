08a297169c3313cb8370f00dc94f6283671be805
Łukasz Niedziałek <intergenixyt@gmail.com>
2024-12-14 13:19:04 +0100

    ekhm

---
+++ android_app/app/src/main/java/com/genix/notifications_example/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.genix.notifications_example
+
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class MainActivity : ComponentActivity() {
+    val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val token: MutableState<String?> = mutableStateOf(null)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task: Task<String> ->
+            token.value = task.result
+        }
+
+        setContent {
+            val clipboardManager: ClipboardManager = LocalClipboardManager.current
+
+            MaterialTheme(
+                // Save your eyes, use dark theme.
+                colorScheme = darkColorScheme(),
+            ) {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(8.dp)
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        TextButton(
+                            onClick = {
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                                    permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                                }
+                            },
+                        ) {
+                            Text("ask for notification permission")
+                        }
+                        Text(
+                            text = "TOKEN"
+                        )
+                        Text(
+                            "${token.value}"
+                        )
+                        TextButton(
+                            onClick = {
+                                token.value?.let {
+                                    clipboardManager.setText(AnnotatedString(it))
+                                }
+                            },
+                        ) {
+                            Text("copy to clipboard")
+                        }
+                        TextButton(
+                            onClick = {
+                                token.value?.let {
+                                    sendToken(it)
+                                }
+                            },
+                        ) {
+                            Text("send")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun sendToken(token: String) = GlobalScope.launch {
+        // This is your localhost:8080,
+        // see: https://stackoverflow.com/a/5495789
+        val url = URL("http://10.0.2.2:8080/token")
+
+        val httpCon: HttpURLConnection = url.openConnection() as HttpURLConnection
+        httpCon.requestMethod = "PUT"
+        val out = OutputStreamWriter(
+            httpCon.outputStream
+        )
+
+        out.write(token)
+        out.close()
+        httpCon.responseCode
+    }
+
+}
---
+++ go_service/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+
+	"firebase.google.com/go"
+	"firebase.google.com/go/messaging"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	tokens := []string{}
+
+	http.HandleFunc("/token", func(writer http.ResponseWriter, request *http.Request) {
+		bytedata, readErr := io.ReadAll(request.Body)
+		if readErr != nil {
+			return
+		}
+
+		newToken := string(bytedata)
+
+		tokens = append(tokens, newToken)
+	})
+
+	http.HandleFunc("/poke", func(writer http.ResponseWriter, request *http.Request) {
+		opt := option.WithCredentialsFile("firebase_key.json")
+		config := &firebase.Config{
+			ProjectID: "medium-notifications-a1df9",
+		}
+		firebaseApp, err := firebase.NewApp(context.Background(), config, opt)
+		if err != nil {
+			return
+		}
+
+		messagingClient, err := firebaseApp.Messaging(context.Background())
+		if err != nil {
+			return
+		}
+
+		for _, deviceToken := range tokens {
+			messagingClient.Send(
+				context.Background(),
+				&messaging.Message{
+					Token: deviceToken,
+					Notification: &messaging.Notification{
+						Title: "TITLE",
+						Body:  "BODY",
+					},
+				},
+			)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
